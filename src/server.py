9a5ef52e6972e3d86c0e659c42ef9d76a98cb14d
Ian Cotter-Llewellyn <ian.llewellyn@toptal.com>
2022-02-07 13:18:02 +0000

    Read the patch list from a file instead of hardcoding it.

--- src/server.py
+++ src/server.py
@@ -2,8 +2,13 @@ from flask import Flask, request
 from base64 import b64encode
 import json, sys
 
-
 server = Flask(__name__)
+
+patch_list = "/code/conf/patchlist.json"
+
+with open(patch_list, 'r') as patch_list_file:
+    patch_list_b64 = b64encode(bytes(
+        patch_list_file.read(), "utf-8")).decode('utf-8')
 
 @server.route("/healthz")
 def healthz():
@@ -15,21 +20,6 @@ def hello():
     #print(request.data, file=sys.stderr)
     admission_review = json.loads(request.data)
     print(admission_review, file=sys.stdout)
-
-    # Deployment, StatefulSet / Pod (this would capture Airflow and GitLab runner initiated Pods
-    # Affinity-Preference and Toleration
-    patch_list = [
-      {"op": "add", "path": "/spec/tolerations/0", "value":
-        {"key": "prismfp.io/environment", "operator": "Equal", "value": "upgrade-test"}
-      },
-    ]
-    patch_list = [
-      {"op": "add", "path": "/spec/tolerations/0", "value":
-        {"key": "prismfp.io/role", "operator": "Equal", "value": "services"}
-      },
-    ]
-
-    patch_list_b64 = b64encode(bytes(json.dumps(patch_list), "utf-8")).decode('utf-8')
 
     # Minimum allowed response stanza
     response = {
@@ -47,5 +37,4 @@ def hello():
 
 if __name__ == "__main__":
     context = ("/certs/tls.crt", "/certs/tls.key")
-    server.run(host='0.0.0.0', ssl_context=context)
-
+    server.run(host='0.0.0.0', ssl_context=context, extra_files=[patch_list])
